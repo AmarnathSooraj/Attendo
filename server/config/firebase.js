309a2e5d549a127bc76aff9a166c4d3ff62f9cfb
AmarnathSooraj <amarnathsooraj7575@gmail.com>
2026-06-04 00:56:28 +0530

    env config

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -1,12 +1,8 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  clientId: process.env.FIREBASE_CLIENT_ID,
-};
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
